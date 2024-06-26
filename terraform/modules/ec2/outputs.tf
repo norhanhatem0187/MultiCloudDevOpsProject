98198c93b5dd4862f2cb64ca5ef5c81ec5bc60e6
norhanhatem0187 <norhan.hatem0187@gmail.com>
2024-06-26 03:31:13 +0300

    Delete terraform directory

--- terraform/modules/ec2/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "instance_id" {
-  description = "The ID of the instance"
-  value       = aws_instance.main.id
-}
-
--- terraform/modules/security-group/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "sg_id" {
-  description = "The ID of the security group"
-  value       = aws_security_group.main.id
-}
-
