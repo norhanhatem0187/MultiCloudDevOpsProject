32b1f7c829edcd5311f598f06ba76373a167f578
norhanhatem0187 <norhan.hatem0187@gmail.com>
2024-06-26 03:36:51 +0300

    Add files via upload

---
+++ terraform/modules/cloudwatch/variable.tf
@@ -0,0 +1,9 @@
+variable "alert_email" {
+  description = "The email address to send alerts to"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name of the EC2 instance to monitor"
+  type        = string
+}
